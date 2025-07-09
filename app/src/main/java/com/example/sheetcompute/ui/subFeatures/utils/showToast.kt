978b150ea72d469ce52478a2572831a9aff4957f
NAGAR <mmm30001230@gmail.com>
2025-07-09 13:41:29 +0300

    Refactor: Improve toast message handling and clean up code structure in fragments

--- app/src/main/java/com/example/sheetcompute/ui/subFeatures/utils/showToast.kt
+++ app/src/main/java/com/example/sheetcompute/ui/subFeatures/utils/showToast.kt
@@ -1,8 +1,11 @@
 package com.example.sheetcompute.ui.subFeatures.utils
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 
  fun showToast(context: Context, message: String) {
     Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-}
+     Log.d("Toast", "Excel import completed: $message")
+
+ }
