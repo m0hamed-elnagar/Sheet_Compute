2d4ea107ef19ebc4b7168442ee516bcf9af6dee0
NAGAR <mmm30001230@gmail.com>
2025-07-21 13:34:44 +0300

    Refactor: Integrate Hilt for dependency injection and update Gradle configurations

--- app/src/main/java/com/example/sheetcompute/ui/features/MainActivity.kt
+++ app/src/main/java/com/example/sheetcompute/ui/features/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.sheetcompute.ui.features
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.sheetcompute.R
 import com.example.sheetcompute.databinding.ActivityMainBinding
+import com.example.sheetcompute.domain.di.MyTestClass
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+    @Inject
+    lateinit var myTestClass: MyTestClass
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Log.d("HiltTest", myTestClass.sayHello())
 
         setupNavController()
         setupToolbarMenu()
